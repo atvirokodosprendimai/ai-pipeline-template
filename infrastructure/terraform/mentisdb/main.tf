1b67aeb4912431d3b75157ae244768d32ec4c37a
Marty <saint@ghost.lt>
2026-04-27 04:50:12 +0200

    fix: server_type cpx21 -> cx23 (cpx21 deprecated for new orders in hel1) (#583)

--- infrastructure/terraform/mentisdb/main.tf
+++ infrastructure/terraform/mentisdb/main.tf
@@ -48,7 +48,7 @@ resource "hcloud_firewall" "mentisdb" {
 resource "hcloud_server" "mentisdb" {
   name         = "mentisdb-prod"
   image        = "ubuntu-24.04"
-  server_type  = "cpx21"
+  server_type  = "cx23"
   location     = "hel1"
   firewall_ids = [hcloud_firewall.mentisdb.id]
   user_data = templatefile("${path.module}/cloud-init.sh.tpl", {
